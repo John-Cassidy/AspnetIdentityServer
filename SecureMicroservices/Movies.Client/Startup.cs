463b7c36717a6e00f5c4280e39950e0ecd92e81d
John Cassidy <jpcassidy@outlook.com>
2021-10-03 09:07:14 -0400

    Section 9 - Hybrid Flow

--- SecureMicroservices/Movies.Client/Startup.cs
+++ SecureMicroservices/Movies.Client/Startup.cs
@@ -43,14 +43,14 @@ namespace Movies.Client {
                 client.DefaultRequestHeaders.Add(HeaderNames.Accept, "application/json");
             });
 
-            //services.AddHttpContextAccessor();
+            services.AddHttpContextAccessor();
 
-            services.AddSingleton(new ClientCredentialsTokenRequest {
-                Address = "https://localhost:5005/connect/token",
-                ClientId = "movieClient",
-                ClientSecret = "secret",
-                Scope = "movieAPI"
-            });
+            //services.AddSingleton(new ClientCredentialsTokenRequest {
+            //    Address = "https://localhost:5005/connect/token",
+            //    ClientId = "movieClient",
+            //    ClientSecret = "secret",
+            //    Scope = "movieAPI"
+            //});
 
             services.AddAuthentication(options =>
             {
@@ -63,7 +63,7 @@ namespace Movies.Client {
 
                     options.ClientId = "movies_mvc_client";
                     options.ClientSecret = "secret";
-                    options.ResponseType = "code";
+                    options.ResponseType = "code id_token";
 
                     options.Scope.Add("openid");
                     options.Scope.Add("profile");
@@ -77,7 +77,7 @@ namespace Movies.Client {
                     //options.ClaimActions.DeleteClaim("auth_time");
                     //options.ClaimActions.MapUniqueJsonKey("role", "role");
 
-                    //options.Scope.Add("movieAPI");
+                    options.Scope.Add("movieAPI");
 
                     options.SaveTokens = true;
                     options.GetClaimsFromUserInfoEndpoint = true;
